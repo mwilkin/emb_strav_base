0128d042f69ad78a006f577ecb6b83dad1a51241
Matthew <matthew.giem@gmail.com>
2016-08-29 16:05:32 -0700

    add a button to each row of the race-bucket display table which allows the user to remove segments they've added from the race

--- app/components/race-bucket-display.js
+++ app/components/race-bucket-display.js
@@ -2,4 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   raceBucket: Ember.inject.service(),
+  actions: {
+    removeFromRace(item) {
+      this.get('raceBucket').delete(item);
+    }
+  }
 });
--- app/services/race-bucket.js
+++ app/services/race-bucket.js
@@ -3,6 +3,8 @@ import Ember from 'ember';
 export default Ember.Service.extend({
   segments: [],
 
+
+
   add(item){
     this.get('segments').pushObject(item);
   },
