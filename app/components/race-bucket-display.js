6ca812f0d7e4452c27b4ab393711fac0787ed320
colson <sumwatt@gmail.com>
2016-08-30 08:49:33 -0700

    add boostrap

--- app/components/race-bucket-display.js
+++ app/components/race-bucket-display.js
@@ -1,13 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  raceBucket: Ember.inject.service(),
   actions: {
     removeFromRace(item) {
       this.get('raceBucket').delete(item);
     },
 
     createRace(){
-      this.sendAction('createRace');
+
+      this.sendAction('createRace', );
     }
   }
 });
