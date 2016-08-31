be7d4afa7022ee769dc49881f8f9a54300918fb6
colson <sumwatt@gmail.com>
2016-08-31 11:34:06 -0700

    fix segment to return promise

--- app/routes/segment.js
+++ app/routes/segment.js
@@ -2,54 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params){
-    // var results = [];
     var store = this.store;
     return store.query('segment', { orderBy: 'race', equalTo: params.race_id}).then(function(segments) {
       var results = [];
+      var promises = [];
       for(var segment of segments.toArray()) {
-        Ember.$.getJSON("/api/user/segment/" + segment.get("ssegid")).then(function(response){
+        promises.push(Ember.$.getJSON("/api/user/segment/" + segment.get("ssegid")));
+      }
+
+      return Promise.all(promises).then(function(responses){
+        responses.forEach(function(response){
           results.push(response);
         });
-      }
-      console.log(results);
-      return results;
-    });
+        return results;
+      });
 
-    //       var results = [];
-    //       var counter = 0;
-    //       for(var s of segmentResult) {
-    //         Ember.$.getJSON("/api/user/segment/" + segmentResult.get("ssegid")).then(function(response){
-    //           results.push(response);
-    //           counter++;
-    //         });
-    //       }
-    //       while(counter !== segmentResult.length) {
-    //       }
-    //       return results;
-    //     });
-    //   }
-    //   return results
-    // });
-      // console.log({segments: results});
-      // return {segments: results};
+    });
   }
-  // model(params){
-  //   var results = [];
-  //   var store = this.store;
-  //   var race = store.findRecord('race', params.race_id);
-  //   race.then(function(){
-  //     var segments = race.get('segments');
-  //     segments.forEach(function(segment){
-  //       var tempSeg = store.findRecord('segment', segment.id);
-  //       tempSeg.then(function(segmentResult){
-  //         Ember.$.getJSON("/api/user/segment/" + segmentResult.get("ssegid")).then(function(response){
-  //           results.push(response);
-  //         });
-  //       });
-  //     });
-  //     console.log({segments: results});
-  //
-  //     return {segments: results};
-  //   });
-  // }
 });
