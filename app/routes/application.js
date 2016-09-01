8e488b7efcd586b987eaecbb548fcd79030c44f9
Ryan Streur <ryan.j.streur@gmail.com>
2016-09-01 10:41:19 -0700

    add a racedetail route and add the user service update functionality to the application route to fix the bug where refreshing any route besides the index route sets the current user to Matt Giem

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  userService: Ember.inject.service(),
   model: function(){
+    var user = this.get('userService');
     return Ember.$.get("/api/user").then(function(response){
+      user.set('user', response.user);
       return {user: response.user};
     });
   }
